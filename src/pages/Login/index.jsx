6775bb57afdfb4cd98440d21e40bd7a6f58d6164
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2021-11-17 14:35:08 -0300

    feat: create form's basic style for login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,36 @@
-import { Container, Box, TextField } from "@mui/material";
+import { Container, Box, TextField, Button, Typography } from "@mui/material";
+import Logo from "../../assets/logo.svg";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
+    const schema = yup.object().shape()
+
+    
   return (
-    <Container component="main" sx={{maxWidth:500}} >
-        <Box component="form">
-            <TextField margin="normal" fullWidth label="Login"/>
-            <TextField margin="normal" fullWidth label="Senha"/>
-        </Box>
-        </Container>
-  )
+    <Container component="main" sx={{ maxWidth: 500 }}>
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          background: `url(${Logo}) no-repeat center`,
+          height: 40,
+        }}
+      ></Box>
+      <Box component="form">
+        <TextField margin="normal" fullWidth label="Login" />
+        <TextField margin="normal" fullWidth label="Senha" type="password" />
+        <Button type="submit" fullWidth variant="contained">
+          Logar
+        </Button>
+        <Typography>
+          Criar uma página para mostrar suas habilidades metas e progresso
+        </Typography>
+        <Button fullWidth variant="contained" sx={{ mt: 2 }}>
+          Cadastrar
+        </Button>
+      </Box>
+    </Container>
+  );
 };
