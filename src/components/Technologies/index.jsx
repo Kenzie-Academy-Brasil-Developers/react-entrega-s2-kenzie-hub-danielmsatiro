4a078a69586988f48b20812f73fe08d0a1492abe
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2021-11-22 08:28:45 -0300

    style: aply changes in card Technologies

--- src/components/Technologies/index.jsx
+++ src/components/Technologies/index.jsx
@@ -1,43 +1,19 @@
-import {
-  Grid,
-  Typography,
-  /* styled, */ ButtonBase,
-  Chip,
-  Box,
-} from "@mui/material";
+import { Grid, Typography, ButtonBase, Chip, Box } from "@mui/material";
 /* import { useState } from "react"; */
 import { FiCodesandbox } from "react-icons/fi";
+import "./style.css";
 
 export const Technologies = ({ id, title, status, handleUpdate }) => {
-  /*   const [color, setColor] = useState("#E7F5EF");
-  const Border = styled("div")(() => ({
-    width: 4,
-    height: 77,
-    backgroundColor: color,
-  }));
-
-  const Icon = styled("div")(() => ({
-    width: 71,
-    height: 77,
-    borderRadius: 5,
-    backgroundColor: color,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  })); */
   return (
     <ButtonBase
-      /* onMouseOver={() => setColor("#11995E")}
-      onMouseOut={() => setColor("#E7F5EF")} */
       onClick={() => handleUpdate(id, title, status)}
       sx={{
         width: "100%",
-        /* "&:hover .hover-tech": {
-          backgroundColor: "#11995E"
-        }, */
+        "&:hover .hover-tech": {
+          backgroundColor: "#11995E",
+        },
         "&:hover .hover-tech--light": {
           color: "#E7F5EF",
-          backgroundColor: "#11995E",
         },
       }}
     >
@@ -47,7 +23,7 @@ export const Technologies = ({ id, title, status, handleUpdate }) => {
             sx={{
               width: 71,
               height: 77,
-              borderRadius: 5,
+              borderRadius: "5px",
               backgroundColor: "#E7F5EF",
               display: "flex",
               justifyContent: "center",
@@ -55,13 +31,7 @@ export const Technologies = ({ id, title, status, handleUpdate }) => {
             }}
             className="hover-tech"
           >
-            <FiCodesandbox
-              className="hover-tech--light"
-              size={30}
-              sx={{
-                color: "#11995E",
-              }}
-            />
+            <FiCodesandbox className="hover-tech--light" size={30} />
           </Box>
         </Grid>
         <Grid item xs>
@@ -82,6 +52,7 @@ export const Technologies = ({ id, title, status, handleUpdate }) => {
                   borderRadius: 1,
                   width: "99px",
                   color: "#11995E",
+                  mt: 2,
                 }}
               />
             </Grid>
